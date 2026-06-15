e2a380f021eec12f49fbf75a0046d731bea5091b
Saldana <jaritza_saldana@student.uml.edu>
2026-06-15 19:34:30 -0400

    Add Javadoc comment

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,9 +3,12 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+/**
+ * Calculator service.
+ */
 @Service
 public class Calculator {
-    final static int NUMBER1 = 3;
+    public final static int NUMBER1 = 3;
 
     @Cacheable("sum")
     public int sum(int a, int b) {
